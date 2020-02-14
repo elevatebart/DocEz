5c6d7358f115d0a5b240113978f85604fb9ac57c
elevatebart <ledouxb@me.com>
2020-02-13 23:30:45 -0600

    first steps of vsg

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // set your styleguidist configuration here
   title: "Default Style Guide",
   // components: "./src/components/**/[A-Z]*.vue",
-  ignore: ["**/Datepicker[A-Z]*.vue", "**/HelloWorld.vue"],
+  ignore: ["**/Datepicker[A-Z]*.vue", "**/HelloWorld.vue", "**/MyButton.vue"],
   defaultExample: true,
   // sections: [
   //   {
